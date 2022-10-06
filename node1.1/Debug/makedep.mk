714f982ba719b5893f9b44098802bbdfaef562cd
sacitas <100199653+sacitas@users.noreply.github.com>
2022-10-06 19:31:57 +0200

    Add files via upload
    Menu improved

---
+++ node1.1/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+adc_driver.c
+
+main.c
+
+oled.c
+
+oled_menu.c
+
+uart.c
+
+xmem.c
+
