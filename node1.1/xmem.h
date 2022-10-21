fa8957bdee79779e05a7a07a2d50bf71557f4a96
Elias Olsen Almenningen <57808408+eliasoa@users.noreply.github.com>
2022-10-21 19:40:46 +0200

    Delete node1.1 directory

--- node1.1/xmem.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-void xmem_init ();
-
-void xmem_write ( uint16_t base_address, int addr, uint8_t data);
-
-uint8_t xmem_read ( uint16_t base_address, uint16_t addr );
