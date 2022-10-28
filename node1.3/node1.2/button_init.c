6378484e9255e7b8c5f21340adfec7da0cd1b35b
Elias Olsen Almenningen <57808408+eliasoa@users.noreply.github.com>
2022-10-28 19:09:50 +0200

    Delete node1.3 directory

--- node1.3/node1.2/button_init.c
+++
@@ -1,7 +0,0 @@
-#include <avr/io.h>
-
-void button_init( void ){
-	SFIOR &= ~(1 << PUD);
-	DDRB &= ~(1<<PB2); // PIN B2 as input
-	PORTB |= (1<<PB2); // Enable internal pull up on PB2
-}
--- node1.3/node1.2/button_init.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <avr/io.h>
-#include <stdio.h> // To use printf
-
-
-void button_init( void );
