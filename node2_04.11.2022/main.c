517f3f70e5647e435ee79656322315b7a793ee91
Elias Olsen Almenningen <57808408+eliasoa@users.noreply.github.com>
2022-11-10 20:07:11 +0100

    Add files via upload

---
+++ node2_04.11.2022/main.c
@@ -0,0 +1,96 @@
+#include "sam.h"
+
+#include "printf-stdarg.h"
+#include "uart.h"
+#include "can_controller.h"
+#include "pwm_driver.h"
+#include "adc_driver.h"
+CAN_MESSAGE message;
+int main(  )
+{
+    SystemInit();
+	WDT->WDT_MR = WDT_MR_WDDIS;	// Watchdog disabled
+	configure_uart();
+	can_setup();
+	pwm_init();
+	uint16_t lol = 0;
+
+	adc_init();
+
+	
+
+
+	while(1){
+
+	//int pos = message.data[1];
+	//servo_get_pos(pos);
+	printf("Read ADC: %d\t\n\r", adc_read());
+	printf("Score is: %d\t\n\r", ir_counter(adc_read()));
+	}
+
+}
+
+// moved interrupt handle to main to be able to read message.() globally
+
+void CAN0_Handler( void )
+{
+	//if(DEBUG_INTERRUPT)printf("CAN0 interrupt\n\r");
+	char can_sr = CAN0->CAN_SR; 
+	
+	//RX interrupt
+	if(can_sr & (CAN_SR_MB1 | CAN_SR_MB2) )//Only mailbox 1 and 2 specified for receiving
+	{
+	
+		if(can_sr & CAN_SR_MB1)  //Mailbox 1 event
+		{
+			can_receive(&message, 1);
+			//printf("\n\r################### NEW EVENT BUF 1 #####################\n\r");
+			//printf("Got message with id: %d\t\n\r", message.id);
+			//printf("Got message with length: %d\t\n\r", message.data_length);
+			//printf("Got message with data:\t%d\n\r",message.data[0]);
+			//printf("Got message with data:\t%d\n\r",message.data[1]);
+			
+		}
+		else if(can_sr & CAN_SR_MB2) //Mailbox 2 event
+		
+		{
+			can_receive(&message, 2);
+		}
+		else
+		{
+			printf("CAN0 message arrived in non-used mailbox\n\r");
+		}
+
+	}
+	
+	if(can_sr & CAN_SR_MB0)
+	{
+		//if(DEBUG_INTERRUPT) printf("CAN0 MB0 ready to send \n\r");
+		
+	//Disable interrupt
+		CAN0->CAN_IDR = CAN_IER_MB0;
+
+	}
+
+	if(can_sr & CAN_SR_ERRP)
+	{
+		//if(DEBUG_INTERRUPT)printf("CAN0 ERRP error\n\r");
+
+	}
+	if(can_sr & CAN_SR_TOVF)
+	{
+		//if(DEBUG_INTERRUPT)printf("CAN0 timer overflow\n\r");
+
+	}
+	
+	NVIC_ClearPendingIRQ(ID_CAN0);
+	//sei();*/
+}
+
+
+
+
+
+//MATCH TQ, bør og være samme verdier i phase seg og prop seg
+
+// to print numberz printf("%d",1);
