e4674fe449b85789ba4e4ca52dc4333fabcb2a3b
Elias Olsen Almenningen <57808408+eliasoa@users.noreply.github.com>
2022-11-16 19:10:19 +0100

    Add files via upload

---
+++ node_1_16.11/xmem.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdint.h>
+
+void xmem_init ();
+
+void xmem_write ( uint16_t base_address, int addr, uint8_t data);
+
+uint8_t xmem_read ( uint16_t base_address, uint16_t addr );
